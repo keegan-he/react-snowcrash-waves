d75e250d398360370c332e1759cf94950bddbaac
Keegan H <keeganslane@gmail.com>
2019-04-30 23:05:18 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react"
+import Wave from 'react-wavify'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="bigwrapper">
+        <div className="hello">
+          <div className="bod">
+            <div className="text">
+              snow crash
+              <div className="a book">
+                a book
+                </div>
+              <div className="a book">
+                by neal stevenson
+                </div>
+            </div>
+            <div className="header2">
+              <Wave fill='#f79902'
+                paused={false}
+                options={{
+                  height: 20,
+                  amplitude: 30,
+                  speed: 0.34,
+                  points: 3
+                }}
+              />
+              <div className="header3">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default App;
